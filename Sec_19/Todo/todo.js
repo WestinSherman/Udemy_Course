b7dc602a6e81ef84aad00b852fb98a5cd9db36a9
WestinSherman <westin@arnst.ca>
2024-10-22 17:33:47 -0700

    Second run through of Todo project.

--- Sec_19/Todo/todo.js
+++ Sec_19/Todo/todo.js
@@ -1,25 +1,26 @@
-let input = prompt("What do you want to do?");
-const todos = ["Conquer Nature", "Create Robot Army"];
+let input = prompt("what would you like to do?");
+const todos = ["Become Omnipotent through Tech.", "Build Robot Army."]
 while (input !== "quit" && input !== "q") {
     if (input === "list") {
-        console.log("*******************")
+        console.log("****************")
         for (let i = 0; i < todos.length; i++) {
             console.log(`${i}: ${todos[i]}`);
         }
-        console.log("*******************")
+        console.log("****************")
     } else if (input === "new") {
-        const newTodo = prompt("Ok, What is the new todo?");
+        const newTodo = prompt("Ok, what would you like to add?");
         todos.push(newTodo);
         console.log(`${newTodo} added to the list!`)
     } else if (input === "delete") {
-        const index = parseInt(prompt("Ok, enter an index to delete:"));
+        const index = parseInt(prompt("Ok, enter index to delete:"));
         if (!Number.isNaN(index)) {
             const deleted = todos.splice(index, 1);
             console.log(`Ok, deleted ${deleted[0]}`);
         } else {
-            console.log("Unknown idnex")
+            console.log("Unknown index")
         }
+
     }
-    input = prompt("What do you want to do?")
+    input = prompt("what would you like to do?")
 }
-console.log("Quiting App")
+console.log("Quiting App.") 
