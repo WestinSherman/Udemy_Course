6217b807ef172d4652038b919ad0f7ed7b911cc8
WestinSherman <westin@arnst.ca>
2024-10-28 17:17:30 -0700

    Completed S-20, moving onto S-21. S-20 was mostly quizes.

---
+++ Sec-20/Functions.js
@@ -0,0 +1,31 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+
+}
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = " ";
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+
+// function add(x, y) {
+//     let sum = x + y;
+//     return sum
+// }
+
+function add(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+        return false;
+    }
+    return x + y;
+}
