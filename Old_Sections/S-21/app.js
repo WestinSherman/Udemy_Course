6a71024e6829aeac6b5704940d802f3aae7934eb
WestinSherman <westin@arnst.ca>
2024-11-04 12:05:35 -0800

    Finished S-22, now working on S-23.

--- Old_Sections/S-21/app.js
+++ Old_Sections/S-21/app.js
@@ -1,60 +1,60 @@
-// function collectEggs(){
-//     let totalEggs = 6;
-//     console.log(totalEggs);
-// }
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
+}
 
-// let bird = "Scarlet Macaw";
-// function birdWatch() {
-//     let bird = "Great Blue Heron";
-// }
-
-
-
-// function callTwice(func) {
-//     func();
-//     func();
-// }
-
-// function callTenTime(f) {
-//     for (let i = 0; i < 10; i++) {
-//         f()
-//     }
-// }
-
-// function rollDie() {
-//     const roll = Math.floor(Math.random() * 6) + 1;
-//     console.log(roll)
-// }
+let bird = "Scarlet Macaw";
+function birdWatch() {
+    let bird = "Great Blue Heron";
+}
 
 
-// const myMath = {
-//     PI: 3.14159,
-//     square: function (num) {
-//         return num * num;
-//     },
-//     cube: function (num) {
-//         return num ** 3;
-//     }
-// }
 
-// const square = {
-//     area: function (side){
-//         return side * side;
-//     },
-//     perimeter: function (side) {
-//         return side * 4;
-//     }
-// }
+function callTwice(func) {
+    func();
+    func();
+}
 
-// const Mac = {
-//     name: "Mactillion",
-//     color: "Grey",
-//     breed: "Apple",
-//     Frrer() {
-//         console.log("THIS IS:", this);
-//         console.log(`${this.name} says Cannot Compute! Cannot Compute! Cannot Compute!`);
-//     }
-// }
+function callTenTime(f) {
+    for (let i = 0; i < 10; i++) {
+        f()
+    }
+}
+
+function rollDie() {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    console.log(roll)
+}
+
+
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
+
+const square = {
+    area: function (side) {
+        return side * side;
+    },
+    perimeter: function (side) {
+        return side * 4;
+    }
+}
+
+const Mac = {
+    name: "Mactillion",
+    color: "Grey",
+    breed: "Apple",
+    Frrer() {
+        console.log("THIS IS:", this);
+        console.log(`${this.name} says Cannot Compute! Cannot Compute! Cannot Compute!`);
+    }
+}
 
 
 function yell(msg) {
---
+++ Old_Sections/S-22/app.js
@@ -0,0 +1,128 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// const doubles = numbers.map(function (num) {
+//     return num * 2;
+// })
+
+
+
+
+
+
+// const newMovies = movies.map(function (movie) {
+
+// numbers.forEach(function (el) {
+//     if (el % 2 === 0)
+//         console.log(el);
+// })
+
+
+// for (let el of numbers) {
+//     console.log(el);
+// }
+
+
+// movies.filter(m => m.score >)
+// const goodMovies = movies.filter(m => m.score > 90)
+// const badMovies = movies.filter(m => m.score < 90)
+// const recentMovies = movies.filter(m => m.year > 2000)
+// const oldMovies = movies.filter(m => m.year < 2000)
+
+
+// const newMovies = movies.map(movie => `${movie.title} - ${movie.score / 10}`)
+
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+
+
+
+
+
+// // DO NOT ALTER THE FOLLOWING CODE:
+// const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+
+// // Write your code here
+// const firstNames = fullNames.map(function (person) {
+//     return person.first;
+// })
+
+// const add = function(x, y) {
+//     return x + y;
+// }
+
+// const add = (x, y) =>  {
+//     return x + y;
+// }
+
+// const greet = (name) => {
+//     return `Hey ${name}`;
+// }
+
+// const greet = name => `Hey ${name}!`;
+
+
+
+
+
+// const movies = [
+//     {
+//         title: "The Matrix",
+//         score: 87,
+//         year: 1999
+//     },
+//     {
+//         title: "Inception",
+//         score: 91,
+//         year: 2010
+//     },
+//     {
+//         title: "Interstellar",
+//         score: 74,
+//         year: 2014
+//     },
+//     {
+//         title: "Parasite",
+//         score: 96,
+//         year: 2019
+//     },
+//     {
+//         title: "Joker",
+//         score: 69,
+//         year: 2019
+//     },
+//     {
+//         title: "Toy Story",
+//         score: 95,
+//         year: 1995
+//     },
+//     {
+//         title: "Avatar",
+//         score: 82,
+//         year: 2009
+//     },
+//     {
+//         title: "Titanic",
+//         score: 89,
+//         year: 1997
+//     },
+//     {
+//         title: "Finding Nemo",
+//         score: 93,
+//         year: 2003
+//     },
+//     {
+//         title: "The Godfather",
+//         score: 100,
+//         year: 1972
+//     }
+// ];
+
+
+// moveies.some(movie => movie.year > 2015);
+
+
+// const exams = [80, 98, 92, 78, 72, 90, 89, 84, 81, 77]
+
+// exams.every(score => score >= 70)
+
+
