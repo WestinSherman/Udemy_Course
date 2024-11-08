e845044c1bb89708be87dd7e9625cfb9040181c6
WestinSherman <westin@arnst.ca>
2024-11-08 14:02:42 -0800

    Finally finished S-24, Starting S-25/Dom Events

---
+++ Old_Sections/S-24 DOM/DOM_Intro_CODE/Manipulating/app.js
@@ -0,0 +1,36 @@
+// const allLinks = document.querySelectorAll('a');
+
+
+// // for (let link of allLinks) {
+// //     link.innerText = 'I AM A LINK!!!!'
+// // }
+
+
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+
+
+
+// const allImages = document.getElementsByTagName("img");
+
+// for (let img of allImages) {
+//     // console.log(img.src)
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg"
+// }
+
+// document.getElementsByTagName("p")
+// document.getElementsByClassName("square")
+
+// const squareImages = document.getElementsByClassName("square");
+
+// for (let img of squareImages) {
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg"
+// }
+
+// const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = "I AM A LINK!!!!"
+// }
