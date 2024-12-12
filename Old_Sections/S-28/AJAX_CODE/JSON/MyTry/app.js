8976533a0239a093e5a89be8210a0d75fcfb743a
WestinSherman <westin@arnst.ca>
2024-12-12 15:19:49 +0100

    Just finished S-28, learned about JSON. Now starting S-29.

---
+++ Old_Sections/S-28/AJAX_CODE/JSON/MyTry/app.js
@@ -0,0 +1,39 @@
+const data = `{
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters":
+		{
+			"batter":
+				[
+					{ "id": "1001", "type": "Regular" },
+					{ "id": "1002", "type": "Chocolate" },
+					{ "id": "1003", "type": "Blueberry" },
+					{ "id": "1004", "type": "Devil's Food" }
+				]
+		},
+	"topping":
+		[
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
+}`
+
+const dog = {
+    "dogBreeds": [{
+        "breed": "Labrador Retriever",
+        "breedType": "Purebred",
+        "origin": "Canada,USA",
+        "popularity": "1",
+        "temperament": ["Cheerful", "Gentle", "Friendly", "Intelligent"],
+        "hypoallergenic": "No",
+        "intelligence": 7,
+        "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+    }]
+}
