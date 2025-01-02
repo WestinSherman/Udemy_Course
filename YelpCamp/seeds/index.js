00917bab52f062d95824d6998e4ae9be8282d5ee
WestinSherman <westin@arnst.ca>
2025-01-02 11:07:32 +0100

    S41 complete, now starting S42.

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Bacon ipsum dolor amet chicken rump doner tenderloin, andouille ground round strip steak t-bone short loin bacon alcatra brisket pastrami pig tail. Venison kevin t-bone spare ribs. Hamburger bacon pork loin kielbasa short loin leberkas burgdoggen doner strip steak landjaeger. Shoulder hamburger t-bone turducken cupim andouille ribeye sirloin porchetta ham pork chop picanha corned beef pancetta.',
+            price
         })
         await camp.save();
     }
